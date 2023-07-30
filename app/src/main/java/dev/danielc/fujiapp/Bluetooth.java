a691b3e2b892a1cb9c87d2434745b48b6b0e24ad
Daniel C <brikbusters@gmail.com>
2023-07-29 20:58:41 -0400

    Add some more bluetooth code All stub code, nothing is functional

--- app/src/main/java/dev/danielc/fujiapp/Bluetooth.java
+++ app/src/main/java/dev/danielc/fujiapp/Bluetooth.java
@@ -12,6 +12,9 @@ import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothProfile;
 import android.content.Intent;
+import 	java.util.UUID;
+import 	java.util.Set;
+import android.content.Context;
 
 public class Bluetooth {
     private BluetoothAdapter adapter;
@@ -25,47 +28,70 @@ public class Bluetooth {
     private String FUJI_GET_BLE_VERSION = "389363E4-712E-4CF2-A72E-BFCF7FB6ADC1";
     private String FUJI_GET_SSID = "BF6DC9CF-3606-4EC9-A4C8-D77576E93EA4";
 
-    // Dummy
     private final BluetoothGattCallback callback = new BluetoothGattCallback() {
+        @Override
         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
             if (newState == BluetoothProfile.STATE_CONNECTED) {
-                //log("Bluetooth Connected");
+                // Bluetooth connected, you can now discover services
+                gatt.discoverServices();
             } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-                //log("Bluetooth disconnected");
-            }
-
-            if (status == BluetoothGatt.GATT_SUCCESS) {
-
+                // Bluetooth disconnected, clean up resources if needed
             }
         }
 
-        public void onMtuChanged(BluetoothGatt gatt, int mtu, int status) {
-            gatt.discoverServices();
-        }
-
+        @Override
         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                // Services discovered, you can now read/write characteristics or enable notifications
 
+                // Example: Reading a characteristic
+                BluetoothGattService service = gatt.getService(UUID.fromString(FUJI_DEVICE_INFO));
+                BluetoothGattCharacteristic characteristic = service.getCharacteristic(UUID.fromString(FUJI_GET_BLE_VERSION));
+                gatt.readCharacteristic(characteristic);
+
+                // Example: Enabling notifications for a characteristic
+                BluetoothGattCharacteristic notifyCharacteristic = service.getCharacteristic(UUID.fromString(FUJI_WIFI_LAUNCH_REQUEST));
+                gatt.setCharacteristicNotification(notifyCharacteristic, true);
+
+                // Example: Writing to a characteristic
+                BluetoothGattCharacteristic writeCharacteristic = service.getCharacteristic(UUID.fromString(FUJI_GET_WIFI_STATE));
+                byte[] dataToWrite = {0x01, 0x02}; // Sample data to write
+                writeCharacteristic.setValue(dataToWrite);
+                gatt.writeCharacteristic(writeCharacteristic);
+            }
         }
 
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                // Characteristic read successfully, handle the data here
+                byte[] data = characteristic.getValue();
+                // Do something with the data
+            }
+        }
+
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                // Characteristic write successful
+            }
+        }
+
+        @Override
         public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
-
+            // Characteristic notification or indication received, handle the updated value here
+            byte[] data = characteristic.getValue();
+            // Do something with the updated data
         }
 
-        public void onCharacteristicRead (BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
-
+        @Override
+        public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                // Descriptor write successful, if you've enabled notifications, this callback will be called
+            }
         }
 
-        public void onCharacteristicWrite (BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
-
-        }
-
-        public void onDescriptorRead (BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
-
-        }
-
-        public void onDescriptorWrite (BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
-
-        }
+        // Implement other callback methods as needed for onDescriptorRead, onMtuChanged, etc.
     };
 
     // Return an Intent that is to be run by the caller
@@ -88,7 +114,19 @@ public class Bluetooth {
         throw new Exception("Bluetooth adapter is disabled");
     }
 
-    public void connectGATT() {
-        // Connected to adapter.getName();
+    public BluetoothDevice getConnectedDevice() {
+        Set<BluetoothDevice> bondedDevices = adapter.getBondedDevices();
+        if (bondedDevices != null) {
+            for (BluetoothDevice device : bondedDevices) {
+                Backend.jni_print("Currently connected to " + device.getName());
+            }
+        }
+        return null; // No connected device found
+    }
+
+    public void connectGATT(Context context) {
+        if (dev != null) {
+            gatt = dev.connectGatt(context, false, callback);
+        }
     }
 }
