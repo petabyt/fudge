e9db45e8ee4bf71e17adb5b8607f4baf46ac18f8
Daniel C <brikbusters@gmail.com>
2025-07-22 00:57:59 -0400

    Update to sdk 35, opt out of edge-to-edge for all activities

--- android/app/src/main/java/dev/danielc/fujiapp/MainActivity.java
+++ android/app/src/main/java/dev/danielc/fujiapp/MainActivity.java
@@ -11,6 +11,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
