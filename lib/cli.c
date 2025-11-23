6433b354926e5481342e17a0cdc891533011f8ea
Daniel C <brikbusters@gmail.com>
2025-11-22 23:17:50 -0500

    Add libfudge-cli program to replace ci/test program in desktop app TODO: completely remove all test/jank/ci code from desktop utility, then separate it from this repo

---
+++ lib/cli.c
@@ -0,0 +1,236 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <libpict.h>
+#include <fuji.h>
+#include <app.h>
+#include <fp.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+void plat_dbg(char *fmt, ...) {
+	printf("DBG: ");
+	va_list args;
+	va_start(args, fmt);
+	vprintf(fmt, args);
+	va_end(args);
+}
+char *app_get_client_name(void) {
+	return strdup("app");
+}
+void app_print(char *fmt, ...) {
+	printf("APP: ");
+	va_list args;
+	va_start(args, fmt);
+	vprintf(fmt, args);
+	va_end(args);
+}
+void app_send_cam_name(const char *name) {}
+int app_get_os_network_handle(struct NetworkHandle *h) {
+	return 0;
+}
+int app_get_wifi_network_handle(struct NetworkHandle *h) {
+	return -1;
+}
+int app_bind_socket_to_network(int fd, struct NetworkHandle *h) {
+	return 0;
+}
+void tester_log(char *fmt, ...) {
+	printf("LOG: ");
+	va_list args;
+	va_start(args, fmt);
+	vprintf(fmt, args);
+	va_end(args);
+}
+void tester_fail(char *fmt, ...) {
+	printf("FAIL: ");
+	va_list args;
+	va_start(args, fmt);
+	vprintf(fmt, args);
+	va_end(args);
+}
+int fuji_discovery_check_cancel(void *arg) {
+	return 0;
+}
+void fuji_discovery_update_progress(void *arg, enum DiscoverUpdateMessages progress) {}
+void app_increment_progress_bar(int read) {}
+void app_report_download_speed(long time, size_t size) {}
+void app_downloaded_file(const struct PtpObjectInfo *oi, const char *path) {}
+void app_get_file_path(char buffer[256], const char *filename) {abort();}
+void app_downloading_file(const struct PtpObjectInfo *oi) {}
+int app_check_thread_cancel(void) {return 0;}
+void app_get_tether_file_path(char buffer[256]) {abort();}
+
+int fuji_test_discovery(struct PtpRuntime *r);
+
+void nothing(int x) {}
+
+pid_t child_pid = -1;
+
+int fudge_usb_connect(struct PtpRuntime *r, int num) {
+	static int attempts = 0;
+	int rc = fujiusb_try_connect(r, num);
+	if (rc) {
+		const char *msg = "No Fujifilm device found";
+		if (attempts) {
+			printf("%s (%d)\n", msg, attempts);
+		} else {
+			printf("%s\n", msg);
+		}
+		attempts++;
+		return rc;
+	}
+	attempts = 0;
+
+	return 0;
+}
+
+int fudge_dump_usb(int devnum) {
+	struct PtpRuntime *r = ptp_new(PTP_USB);
+	int rc = fudge_usb_connect(r, devnum);
+	if (rc) return rc;
+
+	rc = fujiusb_setup(r);
+	if (rc) return rc;
+
+	printf("Camera mode: ");
+	switch (fuji_get(r)->transport) {
+	case FUJI_FEATURE_RAW_CONV:
+		printf("USB RAW CONV./BACKUP RESTORE");
+		break;
+	case FUJI_FEATURE_USB_CARD_READER:
+		printf("USB CARD READER");
+		break;
+	case FUJI_FEATURE_USB_TETHER_SHOOT:
+		printf("USB TETHER SHOOTING");
+		break;
+	case FUJI_FEATURE_USB:
+		printf("MTP");
+		break;
+	default:
+		printf("Invalid %d", fuji_get(r)->transport);
+		break;
+	}
+	printf("\n");
+
+	struct PtpDeviceInfo oi;
+	rc = ptp_get_device_info(r, &oi);
+	if (rc) return rc;
+
+	char buffer[4096];
+	ptp_device_info_json(&oi, buffer, sizeof(buffer));
+	printf("%s\n", buffer);
+
+	for (int i = 0; i < oi.props_supported_length; i++) {
+		printf("Get prop value %04x\n", oi.props_supported[i]);
+		rc = ptp_get_prop_value(r, oi.props_supported[i]);
+		if (rc == PTP_CHECK_CODE) continue;
+		if (rc) return rc;
+		printf("%04x = %x\n", oi.props_supported[i], ptp_parse_prop_value(r));
+	}
+
+	ptp_close_session(r);
+	ptp_device_close(r);
+
+	return rc;
+}
+
+int fudge_test_camera(const char *name) {
+	signal(SIGUSR1, nothing);
+
+	char thispid[16];
+	sprintf(thispid, "%d", getpid());
+	const char *ip_addr = "0.0.0.0";
+
+	child_pid = fork();
+	if (child_pid == -1) {
+		perror("fork failed");
+		exit(1);
+	}
+
+	if (child_pid == 0) {
+		int rc = execlp("vcam", "vcam", name, "tcp", "--ip", ip_addr, "--sig", thispid, NULL);
+		printf("Return value: %d\n", rc);
+		exit(0);
+	}
+
+	printf("Waiting for vcam signal...\n");
+	pause();
+
+	int rc = 0;
+
+	struct PtpRuntime *r = ptp_new(PTP_IP_USB);
+	fuji_reset_ptp(r);
+	fuji_get(r)->transport = FUJI_FEATURE_WIRELESS_COMM;
+	if (ptpip_connect(r, ip_addr, FUJI_CMD_IP_PORT, 0)) {
+		printf("Error connecting to %s:%d\n", ip_addr, FUJI_CMD_IP_PORT);
+		return -1;
+	}
+	
+	rc = fuji_test_setup(r);
+	if (rc) return rc;
+	
+	rc = fuji_test_filesystem(r);
+	if (rc) return rc;
+	
+	rc = ptp_close_session(r);
+	if (rc) return rc;
+	
+	ptpip_device_close(r);
+
+	printf("Going to kill %d\n", child_pid);
+	if (kill(child_pid, SIGINT) == -1) {
+		perror("kill failed");
+		return -1;
+	}
+
+	wait(NULL);
+	printf("Child process terminated\n");
+	return 0;
+}
+
+int fudge_test_all_cameras(void) {
+	const char *models[] = {"fuji_x_h1", "fuji_x_a2", "fuji_x_t20", "fuji_x_t2", "fuji_x_s10", "fuji_x_f10", "fuji_x30", "fuji_x_dev"};
+	const int len = sizeof(models) / sizeof(models[0]);
+
+	for (int i = 0; i < len; i++) {
+		int rc = fudge_test_camera(models[i]);
+		if (rc) return rc;
+	}
+
+	return 0;
+}
+
+
+static int help(void) {
+	printf("TODO: help\n");
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	int devnum = -1;
+	for (int i = 1; i < argc; i++) {
+		if (!strcmp(argv[i], "--test-wifi")) {
+			int rc = fudge_test_all_cameras();
+			plat_dbg("Result: %d\n", rc);
+			return rc;
+		}
+		if (!strcmp(argv[i], "--dump-usb")) {
+			return fudge_dump_usb(devnum);
+		}
+		if (!strcmp(argv[i], "--test-discovery")) {
+			fuji_test_discovery(ptp_new(PTP_USB));
+			return 1;
+		}
+		if (!strcmp(argv[i], "--help")) {
+			return help();
+		}
+		printf("Invalid arg '%s'\n", argv[i]);
+		return -1;
+	}
+
+	return 0;
+}
---
+++ lib/jank.c
@@ -0,0 +1,291 @@
+// Jank code from testing Fuji PTP
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <time.h>
+#ifdef WIN32
+	#include <winsock2.h>
+	#include <ws2tcpip.h>
+#else
+	#include <poll.h>
+	#include <arpa/inet.h>
+#endif
+#include <libpict.h>
+#include <fuji.h>
+#include <app.h>
+#include <fujiptp.h>
+
+// Trying to set camera properties like Fuji tetherapp does.
+int fudge_test(struct PtpRuntime *r) {
+	if (ptp_device_init(r)) {
+		plat_dbg("Device connection error");
+		return 0;
+	}
+
+	int rc;
+
+	ptp_open_session(r);
+
+#if 0
+	struct PtpPropDesc pd;
+	rc = ptp_get_prop_desc(r, 0xd20b, &pd);
+
+	printf("Prop data type: %d\n", ptp_get_payload_length(r));
+
+	rc = ptp_get_prop_value(r, 0xD21c);
+	if (rc) return rc;
+
+	printf("Prop code: %x\n", ptp_parse_prop_value(r));
+#endif
+
+/*
+Fuji Wireless/USB Tether:
+- PtpGetPropValue must be called after PtpSetPropValue
+- If menu (or quick menu) is open, PtpSetPropValue will return DeviceBusy
+- PtpDeviceInfo seems to include some data to indicate what properties have changed (bitmap?)
+- PtpDeviceInfo FunctionalState is 0x02a8 (can't remember, vendor?)
+- AFAIK gphoto people have not documented this mode. There were a few github issues about brokenness.
+*/
+
+// 		char buffer[90000];
+// 		int i = 0;
+// 		while (1) {
+// 			ptp_set_prop_value(r, 0x5015, entries[i % 8]);
+// 			i++;
+// 			ptp_get_prop_value(r, 0x5015);
+// 		
+// 			struct PtpDeviceInfo di;
+// 			rc = ptp_get_device_info(r, &di);
+// 			if (rc) return rc;
+// 			ptp_device_info_json(&di, buffer, sizeof(buffer));
+// 			printf("%s\n", buffer);
+// 
+// 			ptp_get_prop_value(r, 0xd20b);
+// 			//printf("%X: %d\n", 0xd20b, ptp_parse_prop_value(r));
+// 			ptp_get_prop_value(r, 0xd212);
+// 			//printf("%X: %d\n", 0xd212, ptp_parse_prop_value(r));
+// 
+// 
+// 			usleep(1000 * 1000 * 2);
+// 		}
+
+	return 0;
+}
+
+// Fuji sets this property while doing pc autosave
+// guess: client tells camera what photos are already downloaded
+int _fuji_d228(struct PtpRuntime *r) {
+	char buffer[64];
+	int s = 0;
+	s += ptp_write_u8(buffer + s, 6);
+	s += ptp_write_u32(buffer + s, 0x0020);
+	s += ptp_write_u32(buffer + s, 0x0030);
+	s += ptp_write_u32(buffer + s, 0x002f);
+	s += ptp_write_u32(buffer + s, 0x0036);
+	s += ptp_write_u32(buffer + s, 0x0030);
+	s += ptp_write_u32(buffer + s, 0x0000);
+	return ptp_set_prop_value_data(r, 0xd228, buffer, s);
+}
+
+/*
+- GetPartialObject and GetObjectInfo both refuse to send response if conditions are wrong
+- Most of the time we can recover
+- Is there a way to determine if we shouldn't download an image? No, ObjectInfo is basically the same.
+- The max doesn't seem to matter.
+- It works fine if we download the entire image. If we don't, or try and skip to the end, the next getpartialobject/getobjectinfo returns no
+response.
+
+Conclusion: GetPartialObject tracks the number of bytes read, and freaks out when attemping another object with previous unfinished
+*/
+int fuji_autosave_thumb(struct PtpRuntime *r, int handle) {
+	int rc;
+
+	plat_dbg("Object #%d...", handle);
+#if 0
+	struct PtpObjectInfo oi;
+	r->wait_for_response = 1;
+	rc = ptp_get_object_info(r, handle, &oi);
+	if (rc == PTP_CHECK_CODE) {
+		return 0;
+	}
+	if (rc) {
+		plat_dbg("Failed to get object info: %d", rc);
+		return 0;
+	} else {
+		plat_dbg("Got object info");
+	}
+
+	char buffer[1024];
+	ptp_object_info_json(&oi, buffer, sizeof(buffer));
+	plat_dbg(buffer);
+#endif
+
+//	rc = ptp_get_partial_object(r, handle, 0, 0x0);
+//	rc = ptp_get_partial_object(r, handle, 0, 0x0);
+
+//	_fuji_d228(r);
+
+	//rc = ptp_set_prop_value16(r, PTP_PC_FUJI_NoCompression, 1);
+
+	 rc = fuji_get_events(r);
+	 if (rc) return rc;
+
+
+	int max = 0x100000/128;
+
+//	if (handle == 15 || handle == 8 || handle == 7) max = 0x100000;
+
+	r->wait_for_response = 2;
+	rc = ptp_get_partial_object(r, handle, 0, max);
+	if (rc == PTP_IO_ERR) return rc;
+	//rc = ptp_get_partial_object(r, handle, 0xfffffff, 0xfffffff);
+
+	rc = ptp_get_partial_object(r, handle, max, max * 2);
+	if (rc == PTP_IO_ERR) return rc;
+
+	 rc = fuji_get_events(r);
+	 if (rc) return rc;
+	 rc = fuji_get_events(r);
+	 if (rc) return rc;
+	 rc = fuji_get_events(r);
+	 if (rc) return rc;
+	 rc = fuji_get_events(r);
+	 if (rc) return rc;
+
+	/*
+	300-200
+	get event x1
+	max = 0x100000/32
+	partial object x2
+	get event x4
+	Took 67.956009 seconds
+	- x4 -> x1: crash
+	- x1/x3: 63.188999
+	- x1/x2: 56.750000
+	- x1/x1: crash
+	- getobjinfo+x1/x3: 73... without getobjectinfo, 62.955002
+	- get event x1/x1, usleep 100ms: 61.487000
+	- 0 get event, 300ms: crash
+	*/
+
+	//
+
+//	rc = ptp_get_partial_object(r, handle | (1 << 31), 0, 0x0);
+//	rc = ptp_get_partial_object(r, handle | (1 << 31), 0, 0x0);
+
+//	rc = fuji_get_events(r);
+//	if (rc) return rc;
+
+	//rc = ptp_get_partial_object(r, handle, 0x0fffffff, 0x0);
+
+	//rc = ptp_get_partial_object(r, handle, oi.compressed_size - 0x100000, 0x100000);
+	//if (rc == PTP_IO_ERR) return rc;
+
+	//rc = ptp_set_prop_value16(r, PTP_PC_FUJI_NoCompression, 1);
+	//rc = fuji_get_events(r);
+
+	return 0;
+}
+
+int bench_exif_thumb(struct PtpRuntime *r) {
+	float startTime = (float)clock() / CLOCKS_PER_SEC;
+
+	int max = fuji_get(r)->num_objects;
+	for (int i = max; i != max - 20; i--) {
+		int rc = fuji_autosave_thumb(r, i);
+		if (rc) {
+			plat_dbg("Failed autosave thumb");
+			return rc;
+		}
+	}
+
+	float endTime = (float)clock()/CLOCKS_PER_SEC;
+	
+	plat_dbg("Took %f seconds", endTime - startTime);
+
+	return 0;
+}
+
+int handle_add(void *arg, void *data, int size, int read) {
+	fwrite(data, size, 1, (FILE *)arg);
+	return 0;
+}
+
+int try_download(struct PtpRuntime *r) {
+	//plat_dbg("%d", fuji_get(r)->num_objects);
+	int id = 1;
+
+	bench_exif_thumb(r);
+
+	struct PtpObjectInfo oi;
+	int rc = ptp_get_object_info(r, id, &oi);
+	if (rc) {
+		return rc;
+	}
+
+	char buffer[1024];
+	ptp_object_info_json(&oi, buffer, sizeof(buffer));
+	plat_dbg(buffer);
+
+	FILE *f = fopen("TEST", "w");
+	fuji_download_file(r, id, oi.compressed_size, handle_add, (void *)f);
+
+	plat_dbg("Done downloading file");
+
+	return 0;
+}
+
+/*
+Fuji Wireless Tether property setting:
+- PtpGetPropValue must be called after PtpSetPropValue
+- If menu (or quick menu) is open, PtpSetPropValue will return DeviceBusy
+- PtpDeviceInfo seems to include some data to indicate what properties have changed
+*/
+static int dump_prop(struct PtpRuntime *r) {
+	int x = 0x123;
+	struct PtpPropDesc pd;
+	int rc = ptp_get_prop_desc(r, 0x5015, &pd);
+	if (rc) return rc;
+	char buffer[70000];
+	ptp_prop_desc_json(&pd, buffer, sizeof(buffer));
+	printf("%s\n", buffer);
+	return 0;
+}
+
+int fuji_test_discovery(struct PtpRuntime *r) {
+	struct DiscoverInfo info = {0};
+
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+	int rc = fuji_discover_thread(&info, "Fudge (desktop)", NULL);
+	if (rc == FUJI_D_REGISTERED) {
+		plat_dbg("Registered %s %s", info.camera_name, info.camera_model);
+	} else if (rc == FUJI_D_GO_PTP) {
+		usleep(100000); // TODO: Kind of have to wait before connecting
+
+		printf("connecting to %s:%d\n", info.camera_ip, info.camera_port);
+		if (ptpip_connect(r, info.camera_ip, info.camera_port, 0)) {
+			printf("Error connecting to %s:%d\n", info.camera_ip, info.camera_port);
+			return 0;
+		}
+
+		fuji_reset_ptp(r);
+		r->connection_type = PTP_IP_USB;
+		fuji_get(r)->transport = info.transport;
+		strcpy(fuji_get(r)->ip_address, info.camera_ip);
+
+		//rc = fujitether_setup(r);
+		rc = fuji_setup(r);
+		if (rc) return rc;
+
+		try_download(r);
+
+		ptpip_device_close(r);
+	} else {
+		plat_dbg("Response code: %d\n", rc);
+	}
+
+	return -1;
+}
