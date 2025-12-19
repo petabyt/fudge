09f47dcba0d7ff06f02725079edd2a779f21bc45
Daniel C <brikbusters@gmail.com>
2025-12-19 13:09:21 -0500

    remove old makefiles

--- ndk/Lua.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LUA := $(LOCAL_PATH)/../third_party/lua
-LOCAL_MODULE := liblua
-LOCAL_CFLAGS := -O2
-LOCAL_SRC_FILES := $(addprefix $(LUA)/,lbaselib.c lauxlib.c lapi.c lcode.c lctype.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c ltm.c lundump.c lvm.c lzio.c ltests.c)
-LOCAL_C_INCLUDES += $(LUA)
-
-include $(BUILD_STATIC_LIBRARY)
